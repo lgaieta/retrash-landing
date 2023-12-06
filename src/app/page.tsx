803278eab5af3553153ba04374969088de865d37
lgaieta <lgaieta.dev@gmail.com>
2023-12-06 17:47:00 -0300

    Added colors to palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { kanit } from './font'
 
 export default function Home () {
-  return <main className='m-10'>
-      <h1 className={`text-3xl font-bold ${kanit.className}`}>ReTrash</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime modi, fuga minima optio enim labore obcaecati molestiae natus ad ipsum!</p>
+  return <main className='m-10 bg-background'>
+      <h1 className={`text-3xl font-bold ${kanit.className} text-foreground`}>ReTrash</h1>
+      <p className='text-foreground-light'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime modi, fuga minima optio enim labore obcaecati molestiae natus ad ipsum!</p>
+      <button className='bg-accent text-white p-2 text-base rounded-xl'>Comprar ya</button>
     </main>
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,18 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        foreground: '#161616',
+        background: '#ffffff',
+        accent: '#0A801D',
+        'foreground-light': '#545454'
+      }
+    }
+  },
+  plugins: []
 }
 export default config
