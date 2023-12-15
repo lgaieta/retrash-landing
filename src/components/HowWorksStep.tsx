36744f5469fee08ce6118cb5867de376da7b8b33
lgaieta <lgaieta.dev@gmail.com>
2023-12-15 14:05:58 -0300

    Added HowWorksSection for mobile

--- src/components/HowWorksStep.tsx
+++ src/components/HowWorksStep.tsx
@@ -1,15 +1,25 @@
+import { Card, CardBody } from '@nextui-org/card';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import Image from 'next/image';
+
 type HowWorksStepProps = {
   title: string;
   description: string;
-  iconSrc: string;
+  iconSrc: string | StaticImport;
 };
 
 function HowWorksStep(props: HowWorksStepProps) {
   return (
-    <li className='flex flex-col'>
-      <div className='mb-4 h-10 w-10 rounded-full bg-foreground'></div>
-      <p className='w-full pb-1 text-lg font-bold'>{props.title}</p>
-      <p className='w-full text-foreground-500'>{props.description}</p>
+    <li className='z-10 flex flex-col items-center gap-16'>
+      <div className='flex h-12 w-12 items-center justify-center rounded-full bg-foreground-800'>
+        <Image src={props.iconSrc} alt={props.title + ' icon'} />
+      </div>
+      <Card shadow='md'>
+        <CardBody className='p-8'>
+          <p className='w-full pb-1 text-lg font-bold'>{props.title}</p>
+          <p className='w-full text-foreground-500'>{props.description}</p>
+        </CardBody>
+      </Card>
     </li>
   );
 }
--- src/sections/HowWorksSection.tsx
+++ src/sections/HowWorksSection.tsx
@@ -1,38 +1,39 @@
 import HowWorksStep from '@components/HowWorksStep';
 import Image from 'next/image';
+import cameraIconSrc from '@public/camera-icon.svg';
+import bulbIconSrc from '@public/bulb-icon.svg';
+import cogIconSrc from '@public/cog-icon.svg';
 
 function HowWorksSection() {
   return (
-    <section className='max-w-sections mx-auto w-full px-6'>
-      <div className='flex flex-col gap-8'>
-        <h2 className='text-2xl font-bold'>
-          ¿Cómo funciona la separación automática?
-        </h2>
-        <ul className='flex flex-col gap-8'>
-          <HowWorksStep
-            title='Captura de datos con sensores'
-            description='Sensores avanzados recopilan datos detallados sobre cada residuo, incluyendo forma, tamaño y composición.'
-            iconSrc=''
-          />
-          <HowWorksStep
-            title='Inteligencia artificial para clasificar'
-            description='La inteligencia artificial analiza los datos, clasificando de manera precisa cada elemento como reciclable o no reciclable.'
-            iconSrc=''
-          />
-          <HowWorksStep
-            title='Separación con compuertas y motores'
-            description='Motores y compuertas internas se activan para separar automáticamente los residuos según corresponda.'
-            iconSrc=''
-          />
-        </ul>
-      </div>
-      <Image
-        src=''
-        alt='Imagen de ReTrash en una casa'
-        className='rounded-2xl object-cover'
-        fill
-        priority
-      />
+    <section className='max-w-sections mx-auto flex w-full flex-col gap-16 px-6 py-20'>
+      <h2 className='text-center text-3xl font-bold'>
+        ¿Cómo funciona la separación automática?
+      </h2>
+      <ul className='relative flex flex-col items-center gap-16'>
+        <div
+          className='absolute h-full w-1'
+          style={{
+            background:
+              'repeating-linear-gradient(to bottom, transparent, transparent 10px, #161616 10px, #161616 20px);',
+          }}
+        ></div>
+        <HowWorksStep
+          title='Captura de datos con sensores'
+          description='Sensores avanzados recopilan datos detallados sobre cada residuo, incluyendo forma, tamaño y composición.'
+          iconSrc={cameraIconSrc}
+        />
+        <HowWorksStep
+          title='Inteligencia artificial para clasificar'
+          description='La inteligencia artificial analiza los datos, clasificando de manera precisa cada elemento como reciclable o no reciclable.'
+          iconSrc={bulbIconSrc}
+        />
+        <HowWorksStep
+          title='Separación con compuertas y motores'
+          description='Motores y compuertas internas se activan para separar automáticamente los residuos según corresponda.'
+          iconSrc={cogIconSrc}
+        />
+      </ul>
     </section>
   );
 }
