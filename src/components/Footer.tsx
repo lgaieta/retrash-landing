8df86123a77e7a594f033a834ed6a2a58fabbe7d
lgaieta <lgaieta.dev@gmail.com>
2023-12-16 19:51:57 -0300

    Fixed footer icons and removed unnecesary styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,53 +1,45 @@
 import ReTrashLogoWhite from './ReTrashLogo-White';
 import Image from 'next/image';
+import facebookSrc from '@public/facebook.svg';
+import xSrc from '@public/x.svg';
+import instagramSrc from '@public/instagram.svg';
 
 function Footer() {
   return (
-    <section className='flex justify-center align-middle w-full bg-foreground '>
-      <div className='flex justify-between p-20'>
-        <div>
-          <div className='pt-10'>
-            <ReTrashLogoWhite />
-          </div>
-          <div className='flex justify-between my-10'>
-            <Image
-              src='/public/Facebook.svg'
-              width={32}
-              height={32}
-              alt='Facebook icon'
-              className='h-8 w-8'
-            />
-            <Image
-              src='/public/X.svg'
-              width={32}
-              height={32}
-              alt='X icon'
-              className='h-8 w-8'
-            />
-            <Image
-              src='/public/Instagram.svg'
-              width={32}
-              height={32}
-              alt='Instagram icon'
-              className='h-8 w-8'
-            />
-            <Image
-              src='/public/Linkdin.svxg'
-              width={32}
-              height={32}
-              alt='Linkdin                                                                                                                                                                                                                       icon'
-              className='h-8 w-8'
-            />
-          </div>
+    <section className='flex w-full flex-col items-center gap-8 bg-foreground py-10 md:flex-row md:justify-center md:gap-16'>
+      <div className='flex flex-col items-center gap-6'>
+        <ReTrashLogoWhite />
+        <div className='flex gap-4'>
+          <Image
+            src={facebookSrc}
+            width={32}
+            height={32}
+            alt='Facebook icon'
+            className='h-8 w-8'
+          />
+          <Image
+            src={xSrc}
+            width={32}
+            height={32}
+            alt='X icon'
+            className='h-8 w-8'
+          />
+          <Image
+            src={instagramSrc}
+            width={32}
+            height={32}
+            alt='Instagram icon'
+            className='h-8 w-8'
+          />
         </div>
-        <div className='flex flex-col mx-5 gap-y-3 '>
-          <h3 className='text-xl text-white font-semibold'>Contactos</h3>
-          <a href='retrash@gmail.com' className='text-foreground-200'>
-            retrash@gmail.com
-          </a>
-          <p className='text-white font-semibold'>(54) 11 1234-5678</p>
-          <p className='text-foreground-200'>Monte Grande, Buenos Aires.</p>
-        </div>
+      </div>
+      <div className='flex flex-col items-center gap-6 md:items-start'>
+        <h3 className='text-xl font-semibold text-white'>Contactos</h3>
+        <a href='#' className='text-foreground-200'>
+          retrash@gmail.com
+        </a>
+        <p className='font-semibold text-white'>(54) 11 1234-5678</p>
+        <p className='text-foreground-200'>Monte Grande, Buenos Aires.</p>
       </div>
     </section>
   );
