da39fe80bdf33951266b8bd3c586a39c380e3be2
lgaieta <lgaieta.dev@gmail.com>
2023-12-17 20:17:59 -0300

    Fixed width in FinalSection

--- src/sections/FinalSection.tsx
+++ src/sections/FinalSection.tsx
@@ -2,15 +2,15 @@ import { Button } from '@nextui-org/button';
 
 function FinalSection() {
   return (
-    <section className='max-w-sections mx-auto flex items-center w-full flex-col gap-16 px-2 py-20 lg:py-32'>
-      <div className='  w-1/2 '>
+    <section className='max-w-sections mx-auto flex w-full flex-col items-center gap-16 px-2 py-20 lg:py-32'>
+      <div className='w-full md:w-1/2'>
         <h2 className='text-center text-3xl font-bold md:text-4xl '>
           No esperes más y unite al futuro del reciclaje
         </h2>
-        <p className='text-center text-foreground-600 my-5'>
-          Haz clic en "Comprar ahora" y descubre cómo ReTrash puede transformar
-          tu hogar y contribuir al bienestar del planeta. ¡Simplifica,
-          contribuye y sé parte del cambio hoy!
+        <p className='my-5 text-center text-foreground-600'>
+          Haz clic en &quot;Comprar ahora&quot; y descubre cómo ReTrash puede
+          transformar tu hogar y contribuir al bienestar del planeta.
+          ¡Simplifica, contribuye y sé parte del cambio hoy!
         </p>
         <div className='flex justify-center'>
           <Button
